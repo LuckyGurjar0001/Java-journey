a2068f6e0114157e2c80442a8b0160b91624b082
LuckyGurjar0001 <EMAIL_USED_IN_GITHUB_0001>
2026-01-29 22:43:17 +0530

    math.java

---
+++ math.java
@@ -0,0 +1,18 @@
+public class math {
+    public static void main(String[] args) {
+        // Math : The java math class has many method that allows you to perform the mathematical task on the methods
+        int a = 34;
+        int b = 54;
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.sqrt(81));
+        System.out.println(Math.abs(-34));
+        System.out.println(Math.pow(2,10));
+        System.out.println(Math.round(-4.6));
+        System.out.println(Math.floor(3.60));
+        System.out.println(Math.ceil(5.7));
+        int randomNum = (int)(Math.random() * 101); 
+        System.out.println((randomNum));
+
+    }
+}
